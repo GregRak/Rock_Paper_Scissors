5fc11dd6014320a2eecb828c3f1f863ed83dfda0
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-01-12 23:02:56 +0100

    Layout_ver.01

--- src/main/java/com/kodilla/rpsfx/RockPaperScissors.java
+++ src/main/java/com/kodilla/rpsfx/RockPaperScissors.java
@@ -1,11 +1,15 @@
 package com.kodilla.rpsfx;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 
 public class RockPaperScissors extends Application {
 
@@ -21,10 +25,32 @@ public class RockPaperScissors extends Application {
         BackgroundImage backgroundImage = new BackgroundImage(backImage, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
 
-        Pane backPane = new Pane();
-        backPane.setBackground(background);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.BASELINE_LEFT);
+        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        grid.setHgap(5.5);
+        grid.setVgap(5.5);
+        ColumnConstraints column0 = new ColumnConstraints();
+        column0.setPercentWidth(50);
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(50);
+        RowConstraints row0 = new RowConstraints();
+        row0.setPercentHeight(20);
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(50);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(30);
 
-        Scene scene = new Scene(backPane, 800, 450, Color.BLACK);
+        Button newGame = new Button();
+        newGame.setText("New Game");
+        Button newGame1 = new Button();
+        newGame1.setText("New Game2");
+
+        grid.setBackground(background);
+        grid.add(newGame,1,0);
+        grid.add(newGame1,0,2);
+
+        Scene scene = new Scene(grid, 800, 450, Color.BLACK);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Rock Paper Scissors");
